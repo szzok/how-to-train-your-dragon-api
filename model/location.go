904971d7390fe58f46313bfe0c7a2ce396450586
szok <wtfszok1337@gmail.com>
2025-11-15 11:12:01 +0100

    update

--- model/location.go
+++ model/location.go
@@ -14,4 +14,5 @@ var Locations = []Location{
 	{ID: 4, Name: "Berserker Island", Inhabitants: "Berserker", Description: "The rugged and hostile home island of the volatile Berserker tribe, led by Dagur the Deranged. Known for its aggressive inhabitants and constant tribal conflict."},
 	{ID: 5, Name: "Vanaheim", Inhabitants: "Sentinels", Description: "A mythical, jungle-covered island that serves as the sacred cemetery and final resting place for all dragons. It is permanently guarded by the elder, blind Sentinel dragons."},
 	{ID: 6, Name: "Valka Mountain", Inhabitants: "Valka with dragons", Description: "A secluded ice sanctuary and massive ice cave, carved out by a massive Alpha dragon, the Bewilderbeast. It was the hidden home of Valka and hundreds of rescued dragons for two decades."},
+	{ID: 7, Name: "Outcast Island", Inhabitants: "Outcast Tribe", Description: "The Outcasts are a tribe of Vikings that have been banished from various tribes, particularly the Hooligan Tribe. Its leader is Alvin the Treacherous, a former Hooligan. While originally enemies of Berk, the two tribes eventually made peace and became allies."},
 }
